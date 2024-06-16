10829dc5c6b3f71475f874b5989eca55ff5f04ab
akiakash189 <akiakash189@outlook.com>
2024-06-16 09:08:01 +0000

    Added app.py

---
+++ Python/app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("DUMBO")
+
+if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+def chat_with_user():
+        st.session_state.messages.append({"role": "user", "content": st.session_state.prompt})
